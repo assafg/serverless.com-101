972ce5254f3d8fc55aab7f6c9d0d567c5c471dd0
assafg <assaf@tikalk.com>
2019-11-25 08:12:07 +0200

    Font change

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,117 +11,124 @@ import logo from '../resources/logo.png';
 import './index.css';
 
 const Footer = ({ name, title, date }) => (
-  <footer>
-    <img className="footer__logo" src={logo} alt="Tikal" />
-    <div>{title}</div>
-    <div />
-  </footer>
+    <footer>
+        <img className="footer__logo" src={logo} alt="Tikal" />
+        <div>{title}</div>
+        <div />
+    </footer>
 );
 
 function TemplateWrapper(props) {
-  const NEXT = [13, 32, 39];
-  const PREV = 37;
+    const NEXT = [13, 32, 39];
+    const PREV = 37;
 
-  const doNavigate = useCallback(
-    ({ keyCode }) => {
-      const now = props.data.slide.index;
+    const data = props.data || { slide: { index: 1 } };
 
-      const slidesLength = props.slidesLength;
-      if (now) {
-        if (keyCode === PREV && now === 1) {
-          return false;
-        } else if (NEXT.indexOf(keyCode) !== -1 && now === slidesLength) {
-          return false;
-        } else if (NEXT.indexOf(keyCode) !== -1) {
-          navigate(`/${now + 1}`);
-        } else if (keyCode === PREV) {
-          navigate(`/${now - 1}`);
-        }
-      }
-    },
-    [props.data.slide.index, NEXT, props.slidesLength]
-  );
+    const doNavigate = useCallback(
+        ({ keyCode }) => {
+            const now = data.slide.index;
 
-  const swipeLeft = () => {
-    doNavigate({ keyCode: NEXT[0] });
-  };
+            const slidesLength = props.slidesLength;
+            if (now) {
+                if (keyCode === PREV && now === 1) {
+                    return false;
+                } else if (
+                    NEXT.indexOf(keyCode) !== -1 &&
+                    now === slidesLength
+                ) {
+                    return false;
+                } else if (NEXT.indexOf(keyCode) !== -1) {
+                    navigate(`/${now + 1}`);
+                } else if (keyCode === PREV) {
+                    navigate(`/${now - 1}`);
+                }
+            }
+        },
+        [data.slide.index, NEXT, props.slidesLength]
+    );
 
-  const swipeRight = () => {
-    doNavigate({ keyCode: PREV });
-  };
-
-  useEffect(() => {
-    hljs.initHighlighting();
-    return () => {
-      hljs.initHighlighting.called = false;
+    const swipeLeft = () => {
+        doNavigate({ keyCode: NEXT[0] });
     };
-  });
 
-  useEffect(
-    () => {
-      document.addEventListener('keydown', doNavigate);
+    const swipeRight = () => {
+        doNavigate({ keyCode: PREV });
+    };
 
-      return () => {
-        document.removeEventListener('keydown', doNavigate);
-      };
-    },
-    [props.data.slide.index, doNavigate]
-  );
+    useEffect(() => {
+        hljs.initHighlighting();
+        return () => {
+            hljs.initHighlighting.called = false;
+        };
+    });
 
-  const { location, children, site } = props;
-  return (
-    <div>
-      <Helmet
-        title={`${site.siteMetadata.title} — ${site.siteMetadata.name}`}
-      />
-      <Swipeable onSwipedLeft={swipeLeft} onSwipedRight={swipeRight}>
-        <Transition location={location}>
-          <div id="slide" style={{ width: '100%' }}>
-            {children}
-          </div>
-        </Transition>
-      </Swipeable>
-      <Footer
-        name={site.siteMetadata.name}
-        title={site.siteMetadata.title}
-        date={site.siteMetadata.date}
-      />
-    </div>
-  );
+    useEffect(() => {
+        document.addEventListener('keydown', doNavigate);
+
+        return () => {
+            document.removeEventListener('keydown', doNavigate);
+        };
+    }, [data.slide.index, doNavigate]);
+
+    const { location, children, site } = props;
+    return (
+        <div>
+            <Helmet
+                title={`${site.siteMetadata.title} — ${site.siteMetadata.name}`}
+            >
+                <link
+                    href="https://fonts.googleapis.com/css?family=Quicksand&display=swap"
+                    rel="stylesheet"
+                ></link>
+            </Helmet>
+            <Swipeable onSwipedLeft={swipeLeft} onSwipedRight={swipeRight}>
+                <Transition location={location}>
+                    <div id="slide" style={{ width: '100%', height: '100vh' }}>
+                        {children}
+                    </div>
+                </Transition>
+            </Swipeable>
+            <Footer
+                name={site.siteMetadata.name}
+                title={site.siteMetadata.title}
+                date={site.siteMetadata.date}
+            />
+        </div>
+    );
 }
 // }
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.node,
-  data: PropTypes.object,
+    children: PropTypes.node,
+    data: PropTypes.object,
 };
 
 export default props => (
-  <StaticQuery
-    query={graphql`
-      query IndexQuery {
-        site {
-          siteMetadata {
-            name
-            title
-            date
-          }
-        }
-        allSlide {
-          edges {
-            node {
-              id
+    <StaticQuery
+        query={graphql`
+            query IndexQuery {
+                site {
+                    siteMetadata {
+                        name
+                        title
+                        date
+                    }
+                }
+                allSlide {
+                    edges {
+                        node {
+                            id
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <TemplateWrapper
-        site={data.site}
-        slidesLength={data.allSlide.edges.length}
-        {...props}
-      />
-    )}
-  />
+        `}
+        render={data => (
+            <TemplateWrapper
+                site={data.site}
+                slidesLength={data.allSlide.edges.length}
+                {...props}
+            />
+        )}
+    />
 );
